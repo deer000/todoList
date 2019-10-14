093e9a995f2bec4b453716eeda85a5a2ef346a86
deer000 <camel000k@gmail.com>
2019-10-14 15:48:43 +0900

    Do it

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# porcontent
-MYPORTFOLIOCONTENT
+# Porcontent
+
+MY PORTFOLIO CONTENT
---
+++ kakao/js/main.js
@@ -0,0 +1,110 @@
+// 변수지정
+const navColor = document.querySelector("nav");
+const SHOWING_CLASS = "showing";
+const firstSlide = document.querySelector(".slide-item:first-child");
+const lastSlide = document.querySelector(".slide-item:last-child");
+const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelector(".prev");
+const btn1 = document.querySelector("#btn1");
+const btn2 = document.querySelector("#btn2");
+const btn3 = document.querySelector("#btn3");
+const btn4 = document.querySelector("#btn4");
+const mega1 = document.querySelector(".menu1");
+const mega2 = document.querySelector(".menu2");
+const mega3 = document.querySelector(".menu3");
+const mega4 = document.querySelector(".menu4");
+
+
+const slideNum = document.querySelector(".slide_number");
+const slideItems = document.getElementsByClassName("slide-item");
+
+let count = 1;
+//함수 선언
+
+const nextSlides = () => {
+    const currentSlide = document.querySelector(`.${SHOWING_CLASS}`);
+    if (currentSlide) {
+        currentSlide.classList.remove(SHOWING_CLASS);
+        const nextSlide = currentSlide.nextElementSibling;
+        if (nextSlide) {
+            nextSlide.classList.add(SHOWING_CLASS);
+            count++;
+        } else {
+            firstSlide.classList.add(SHOWING_CLASS);
+            count = 1;
+        }
+    } else {
+        firstSlide.classList.add(SHOWING_CLASS);
+        count = 1;
+    }
+    slideNum.innerHTML = `${count}/${slideItems.length}`;
+};
+
+const prevSlides = () => {
+    const currentSlide = document.querySelector(`.${SHOWING_CLASS}`);
+    if (currentSlide) {
+        currentSlide.classList.remove(SHOWING_CLASS);
+        const prevSlide = currentSlide.previousElementSibling;
+        if (prevSlide) {
+            prevSlide.classList.add(SHOWING_CLASS);
+            count--;
+        } else {
+            lastSlide.classList.add(SHOWING_CLASS);
+            count = 3;
+        }
+    } else {
+        lastSlide.classList.add(SHOWING_CLASS);
+        count = 3;
+    }
+    slideNum.innerHTML = `${count}/${slideItems.length}`;
+};
+
+nextSlides();
+
+nextBtn.addEventListener("click", nextSlides);
+prevBtn.addEventListener("click", prevSlides);
+setInterval(nextSlides, 5000);
+
+btn1.addEventListener("mouseover", () => {
+    mega1.style.display = "flex";
+});
+
+btn1.addEventListener("mouseout", () => {
+    mega1.style.display = "none";
+});
+
+btn2.addEventListener("mouseover", () => {
+    mega2.style.display = "flex";
+});
+
+btn2.addEventListener("mouseout", () => {
+    mega2.style.display = "none";
+});
+
+btn3.addEventListener("mouseover", () => {
+    mega3.style.display = "flex";
+});
+
+btn3.addEventListener("mouseout", () => {
+    mega3.style.display = "none";
+});
+
+btn4.addEventListener("mouseover", () => {
+    mega4.style.display = "flex";
+});
+
+btn4.addEventListener("mouseout", () => {
+    mega4.style.display = "none";
+});
+
+function scrollFunction() {
+    if (document.documentElement.scrollTop > 30) {
+        navColor.style.background = "#fff";
+    } else {
+        navColor.style.background = "none";
+    }
+}
+
+window.onscroll = function() {
+    scrollFunction();
+}
