326e5e5c56a4b50d54f44169a5bb954728d9bfba
deer000 <camel000k@gmail.com>
2019-11-15 18:48:31 +0900

    alexto_1

--- alecto/main.js
+++ alecto/main.js
@@ -1,42 +1,55 @@
 // class모음
 var SHOWINGCLASS = "showing";
 // Element
-var firstSlide = document.querySelector(".slide_idems:first-child")
+var firstSlide = document.querySelector(".slide_idems:first-child");
 var nextBtn = document.querySelector(".next");
-var lastSlide = document.querySelector(".slide_idems:last-child")
+var lastSlide = document.querySelector(".slide_idems:last-child");
 var prevBtn = document.querySelector(".prev");
+var nav = document.querySelector("nav");
 // function
 function nextSlides() {
-    var currentSlide = document.querySelector(`.${SHOWINGCLASS}`);
-    if (currentSlide) {
-        currentSlide.classList.remove(SHOWINGCLASS);
-        var nextSlides = currentSlide.nextElementSibling;
+  var currentSlide = document.querySelector(`.${SHOWINGCLASS}`);
+  if (currentSlide) {
+    currentSlide.classList.remove(SHOWINGCLASS);
+    var nextSlides = currentSlide.nextElementSibling;
 
-        if (nextSlides) {
-            nextSlides.classList.add(SHOWINGCLASS);
-        } else {
-            firstSlide.classList.add(SHOWINGCLASS);
-        }
+    if (nextSlides) {
+      nextSlides.classList.add(SHOWINGCLASS);
     } else {
-        firstSlide.classList.add(SHOWINGCLASS);
+      firstSlide.classList.add(SHOWINGCLASS);
     }
+  } else {
+    firstSlide.classList.add(SHOWINGCLASS);
+  }
 }
 
 function prevSlides() {
-    var currentSlide = document.querySelector(`.${SHOWINGCLASS}`);
-    if (currentSlide) {
-        currentSlide.classList.remove(SHOWINGCLASS);
-        var prevSlides = currentSlide.previousElementSibling;
+  var currentSlide = document.querySelector(`.${SHOWINGCLASS}`);
+  if (currentSlide) {
+    currentSlide.classList.remove(SHOWINGCLASS);
+    var prevSlides = currentSlide.previousElementSibling;
 
-        if (prevSlides) {
-            prevSlides.classList.add(SHOWINGCLASS);
-        } else {
-            lastSlide.classList.add(SHOWINGCLASS);
-        }
+    if (prevSlides) {
+      prevSlides.classList.add(SHOWINGCLASS);
     } else {
-        lastSlide.classList.add(SHOWINGCLASS);
+      lastSlide.classList.add(SHOWINGCLASS);
     }
+  } else {
+    lastSlide.classList.add(SHOWINGCLASS);
+  }
 }
+
+function fixed() {
+  if (document.documentElement.scrollTop > 20) {
+    nav.style.background = "rgba(0,0,0,0.7)";
+  } else {
+    nav.style.background = "none";
+  }
+}
+
+window.onscroll = function() {
+  fixed();
+};
 // playin function
 nextSlides();
 nextBtn.addEventListener("click", nextSlides);
