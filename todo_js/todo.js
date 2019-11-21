fd699c79f799257d9744723d10ee0c801a68e930
deer000 <camel000k@gmail.com>
2019-11-21 21:33:02 +0900

    todoList

---
+++ todo_js/todo.js
@@ -0,0 +1,7 @@
+const form = document.querySelector("form"),
+	todoList = document.querySelector("input"),
+	todoList = document.querySelector("todo");
+	
+	const todo_LS = "todo";
+	
+	let todo = [];
